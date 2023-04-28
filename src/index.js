09a60c250d8a38e4d1f1702ea8c2f4feeab139ee
Nick <Nbarnes0521@gmail.com>
2023-04-28 14:02:39 -0500

    3rd commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './components';
+
+const root = createRoot(document.querySelector('#app'));
+
+root.render(<App />)
+
+
+/*
 const COHORT_NAME = '2301-FTB-ET-WEB-FT'
 
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
 const returned = await fetch(`${BASE_URL}/posts`)
 
+TO MAKE CHANGES "git add ." then "git commit -m "" "
+Then once done "git push"
+*/
