67e719d13244dfe3ada117e1869a11ccc2b7d1fd
Nick <Nbarnes0521@gmail.com>
2023-04-30 18:06:14 -0500

    Posts add to main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import { Register } from './';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { 
+    Register,
+    Posts
+} from './';
 
-function App(){
+function App() {
     const [token, setToken] = useState('');
 
+    // function tokenCheck() {
+    //     if (window.localStorage.getItem('token')) {
+    //         setToken(window.localStorage.getItem('token'));
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     tokenCheck();
+    // }, [])
 
     return (
-        <div>
-    < Register setToken={setToken} />
+    <div>
+        <Routes>
+            <Route 
+                path=""
+                element={<Posts />} 
+            />
+            <Route 
+                path="/register"
+                element={<Register setToken= {setToken} />}
+            />
+        </Routes> 
     </div>
-    )
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './components';
 
 const root = createRoot(document.querySelector('#app'));
 
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
